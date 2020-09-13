307081d3ce85c152fe234c295b5e1ce6b396684d
Marco Madeira <marcowmadeira@gmail.com>
2020-09-13 01:31:48 +0100

    Removida a mesa

--- helper_o.js
+++ helper_o.js
@@ -11,6 +11,10 @@ let historico = {
 }
 
 let cartas_descartadas = []      // Cartas que já foram utilizadas
+let mesa = []                   // Lista de cartas que estão na mesa
+
+let carta_escolhida = [-1, -1]  // Carta escolhida pelo utilizador
+let mesaY = 0
 
 function iniciarJogo() {
     for (let cor = 0; cor < 3; cor++) {
@@ -18,7 +22,8 @@ function iniciarJogo() {
         for (let carta = 0; carta < 8; carta++)
         cartas_descartadas[cor].push(false)
     }
-
+    
+    mesa = [-1, -1, -1, -1, -1]
     adicionarAoHistorico()
 }
 
@@ -40,26 +45,33 @@ function setup() {
     })
 
     // Criar uma tela para desenhar o jogo
-    let canvas = createCanvas(750, ((750/8) * RA_CARTA) * 3 +1)
+    let canvas = createCanvas(750, 430)
     canvas.parent('game')
 }
 
 function draw() {
     // Limpar canvas
-    clear()
-    //background(16, 16, 21)
+    background(16, 16, 21)
 
     // Desenhar todas as cartas para selecionar
-    let LARGURA_CARTA = width/8
+    let LARGURA_CARTA = width/(4*3)
     let ALTURA_CARTA = LARGURA_CARTA * RA_CARTA
     for (let cor = 0; cor < 3; cor++) {
 
         for (let carta = 1; carta <= 8; carta++) {
             // Calcular posição onde a carta deve ser desenhada
             let x, y
-            
-            x = LARGURA_CARTA * (carta-1)
-            y = ALTURA_CARTA * cor;
+            if (carta_escolhida[0] == cor && carta_escolhida[1] == carta) {
+                continue
+                
+            }
+            else if (carta <= 4) {
+                x = LARGURA_CARTA * (carta-1) + (cor * LARGURA_CARTA * 4)
+                y = 0
+            } else {
+                x = LARGURA_CARTA * (carta%5) + (cor * LARGURA_CARTA * 4)
+                y = ALTURA_CARTA
+            }
         
             fill(CORES[cor])
 
@@ -84,29 +96,140 @@ function draw() {
             text(carta, centro.x, centro.y)
         }
     }
-}
 
+
+    // Desenhar cartas na mesa
+    mesaY = ALTURA_CARTA * 2 + (height*.1)
+    LARGURA_CARTA = width / 5
+    ALTURA_CARTA = LARGURA_CARTA * RA_CARTA
+    for (let pos = 0; pos < 5; pos++) {
+
+        // Desenhar local onde ficam as cartas
+        fill(16, 16, 21)
+        stroke(255)
+        strokeWeight(2)
+        let x = pos * LARGURA_CARTA
+        rect(x, mesaY, LARGURA_CARTA, ALTURA_CARTA)
+
+        // Verificar se não existe uma carta nesta posição
+        if (mesa[pos] == -1)
+            continue
+        
+        // Desenhar carta
+        let cor = mesa[pos][0]
+        let valor = mesa[pos][1]
+
+        fill(CORES[cor])
+        stroke(255)
+        strokeWeight(2)
+        rect(x, mesaY, LARGURA_CARTA, ALTURA_CARTA)
+
+        fill(255)
+        textAlign(CENTER, CENTER)
+        textSize(36)
+        stroke(0)
+        strokeWeight(3)
+        let centro = {
+            x: x + (LARGURA_CARTA / 2),
+            y: mesaY + (ALTURA_CARTA / 2)
+        }
+        text(valor, centro.x, centro.y)
+    }
+
+    // Verificar se o utilizador tem uma carta na mão
+    if (carta_escolhida[0] != -1 && carta_escolhida[1] != -1) {
+        let cor = carta_escolhida[0]
+        let valor = carta_escolhida[1]
+
+        let x = mouseX - (LARGURA_CARTA / 2)
+        let y = mouseY - (ALTURA_CARTA / 2)
+
+        fill(CORES[cor])
+        stroke(255)
+        strokeWeight(2)
+        rect(x, y, LARGURA_CARTA, ALTURA_CARTA)
+
+        let centro = {
+            x: x + (LARGURA_CARTA / 2),
+            y: y + (ALTURA_CARTA / 2)
+        }
+        
+        fill(255)
+        textAlign(CENTER, CENTER)
+        textSize(36)
+        stroke(0)
+        strokeWeight(3)
+        text(valor, centro.x, centro.y)
+        
+    }
+}
 
 function mousePressed() {
     // Verificar se o utilizador carregou fora do canvas
     if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height)
         return
+        
 
-    let LARGURA_CARTA = width / 8
+    let LARGURA_CARTA = width/(4*3)
     let ALTURA_CARTA = LARGURA_CARTA * RA_CARTA
 
     // Verificar se o utilizador escolheu uma carta
-    if (mouseY < ALTURA_CARTA * 3) {
+    if (mouseY < ALTURA_CARTA * 2) {
         // Obter carta escolhida pelo utilizador
-        let cor = ~~(mouseY / (height/3)) 
+        let cor = floor(mouseX / (width/3)) 
         
         let x = floor(mouseX / LARGURA_CARTA)
-        let carta = x % 8 + 1
+        let carta = x % 4 + 1
+        if (mouseY > ALTURA_CARTA) carta += 4
+        
+        // Verificar se a carta ja foi utilizada
+        if (cartas_descartadas[cor][carta-1])
+            return
 
-        cartas_descartadas[cor][carta-1] = true;
-        app.a++;
+        carta_escolhida = [cor, carta]
     }
 }
+
+function mouseReleased() {
+    // Verificar se o utilizador tinha uma carta na mão
+    if (carta_escolhida[0] == -1 || carta_escolhida[1] == -1)
+        return
+
+    // Verificar se o utilizador largou a carta fora do canvas
+    if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height)
+        return
+
+    let cor = carta_escolhida[0]
+    let carta = carta_escolhida[1]
+
+    // Posição onde começa a mesa
+    let LARGURA_CARTA = width/5
+    let ALTURA_CARTA = LARGURA_CARTA * RA_CARTA
+
+    if (mouseY > mesaY && mouseY < (mesaY + ALTURA_CARTA)) {
+        
+        // Obter localização escolhida
+        let pos = floor(mouseX / (width/5))
+        
+        if (mesa[pos] != -1) {
+            // Obter carta antiga
+            let carta_antiga = {
+                cor: mesa[pos][0],
+                valor: mesa[pos][1]
+            }
+
+            cartas_descartadas[carta_antiga.cor][carta_antiga.valor-1] = true
+            app.a++
+        }
+        mesa[pos] = carta_escolhida
+        adicionarAoHistorico()
+    }
+
+    //cartas_descartadas[cor][carta-1] = true
+    
+    carta_escolhida = [-1, -1]
+}
+
 
 /* Funções relativas ao historico */
 function limpar() {
@@ -132,6 +255,7 @@ function carregarDoHistorico(pos) {
     let dados = JSON.parse(JSON.stringify(historico.dados[pos]))
 
     cartas_descartadas = dados.cartas_descartadas
+    mesa = dados.mesa
     app.a++
 }
 
@@ -143,6 +267,7 @@ function adicionarAoHistorico() {
         JSON.parse(
             JSON.stringify(
                 {
+                    mesa,
                     cartas_descartadas
                 }
             )
